2ee0c7f7c50f926f706b4712effba0b1e9325543
yeonsuBaek <soothbugger@gmail.com>
2023-04-14 14:13:29 +0900

    style: body 태그 스타일 초기화

--- application/pages/_document.js
+++ application/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
-      <body>
+      <body
+        style={{
+          margin: 0,
+          padding: 0,
+          boxSizing: 'border-box',
+          padding: '0 20px',
+          backgroundColor: '#f1f1f1',
+        }}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
