baa942c617553db7b35b296439bd804504a649cc
yeonsuBaek <soothbugger@gmail.com>
2023-04-15 13:36:13 +0900

    refactor: CorrectSentences 컴포넌트 생성

---
+++ application/components/CorrectSentences.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import styles from '../styles/correctSentences.module.css';
+import Title from '../components/layouts/Title';
+import TextBox from '@/components/layouts/TextBox';
+import Error from '@/components/layouts/Error';
+import { SpeakerSimpleHigh } from '@phosphor-icons/react';
+
+const CorrectSentences = ({ after }) => {
+  const [voiceError, setVoiceError] = useState('');
+
+  const speakEnglish = (text) => {
+    const speechMsg = new SpeechSynthesisUtterance();
+
+    if (
+      typeof SpeechSynthesisUtterance === 'undefined' ||
+      typeof window.speechSynthesis === 'undefined'
+    ) {
+      setVoiceError('이 브라우저는 음성 합성을 지원하지 않습니다.');
+      return;
+    }
+
+    window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
+
+    speechMsg.rate = 1; // 속도: 0.1 ~ 10
+    speechMsg.pitch = 1; // 음높이: 0 ~ 2
+    speechMsg.lang = 'en-US';
+    speechMsg.text = text;
+
+    // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
+    window.speechSynthesis.speak(speechMsg);
+  };
+
+  return (
+    <>
+      <Title title='Correct Sentences' />
+      <TextBox>{after}</TextBox>
+      {voiceError && <Error message={voiceError} />}
+      <button
+        className={styles.voice}
+        onClick={() => speakEnglish(after)}
+        type='button'
+      >
+        <SpeakerSimpleHigh size={28} color='#0f2b46' weight='fill' />
+      </button>
+    </>
+  );
+};
+
+export default CorrectSentences;
--- application/pages/index.js
+++ application/pages/index.js
@@ -1,15 +1,10 @@
 import { useState } from 'react';
-import styles from '../styles/index.module.css';
 import Header from '../components/Header';
-import Title from '../components/layouts/Title';
-import TextBox from '@/components/layouts/TextBox';
-import { SpeakerSimpleHigh } from '@phosphor-icons/react';
-import Error from '@/components/layouts/Error';
 import YourSentences from '@/components/YourSentences';
+import CorrectSentences from '@/components/CorrectSentences';
 
 export default function Home() {
   const [after, setAfter] = useState('');
-  const [voiceError, setVoiceError] = useState('');
 
   const handleSubmitSentence = async (sentence) => {
     try {
@@ -39,42 +34,11 @@ export default function Home() {
     }
   };
 
-  const speakEnglish = (text) => {
-    const speechMsg = new SpeechSynthesisUtterance();
-
-    if (
-      typeof SpeechSynthesisUtterance === 'undefined' ||
-      typeof window.speechSynthesis === 'undefined'
-    ) {
-      setVoiceError('이 브라우저는 음성 합성을 지원하지 않습니다.');
-      return;
-    }
-
-    window.speechSynthesis.cancel(); // 현재 읽고있다면 초기화
-
-    speechMsg.rate = 1; // 속도: 0.1 ~ 10
-    speechMsg.pitch = 1; // 음높이: 0 ~ 2
-    speechMsg.lang = 'en-US';
-    speechMsg.text = text;
-
-    // SpeechSynthesisUtterance에 저장된 내용을 바탕으로 음성합성 실행
-    window.speechSynthesis.speak(speechMsg);
-  };
-
   return (
     <>
       <Header />
       <YourSentences onSubmit={handleSubmitSentence} />
-      <Title title='Correct Sentences' />
-      <TextBox>{after}</TextBox>
-      {voiceError && <Error message={voiceError} />}
-      <button
-        className={styles.voice}
-        onClick={() => speakEnglish(after)}
-        type='button'
-      >
-        <SpeakerSimpleHigh size={28} color='#0f2b46' weight='fill' />
-      </button>
+      <CorrectSentences after={after} />
     </>
   );
 }
